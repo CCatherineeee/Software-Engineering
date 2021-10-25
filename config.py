ea3d63543cfe9577343c1c554c19a0c6b73e169d
CCatherineeee <76059027+CCatherineeee@users.noreply.github.com>
2021-10-25 21:51:56 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DB_URI = 'mysql+pymysql://root:mysqlroot@39.107.51.181:3306/Education_Management_System'
+DB_URI = 'mysql+pymysql://root:*****@******:3306/Education_Management_System'
 # 'mysql+pymysql://用户名称:密码@localhost:端口/数据库名称'
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
