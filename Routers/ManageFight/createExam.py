19d7e7faebdef092f36e539d40a71cd8f217d2d7
CCatherineee <2870375520@qq.com>
2021-12-11 00:06:17 +0800

    sigleton mysql connect

--- Routers/ManageFight/createExam.py
+++ Routers/ManageFight/createExam.py
@@ -2,12 +2,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # 解决跨域的问题
 from flask import Blueprint
 import json
-from Model.Model import Course,Class,Exam,Question,StudentClass,ExamGroup
+from Model.Model import Course,Class,Exam,Question,StudentClass,ExamGroup,StudentExam
 import dbManage
 from Routers import Role
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 import datetime
 import random
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from Routers import Role
 
 createFightRoute = Blueprint('createFightRoute', __name__)
 
@@ -27,6 +29,24 @@ def createExam():
     exam = Exam(class_id = class_id, title = title, start_time = start_time, end_time = end_time, status = 0)
     dbManage.db.session.add(exam)
     dbManage.db.session.commit()
+
+    # 找到班级学生，生产测验小组
+    students = StudentClass.query.filter(StudentClass.class_id == class_id).all()
+    while(len(students) >= 3):
+        sg = random.sample(students,3)
+        students.remove(sg[0])
+        students.remove(sg[1])
+        students.remove(sg[2])
+        ag = ExamGroup(exam_id = exam.exam_id,s_id_1 = sg[0].s_id,s_id_2 = sg[1].s_id,s_id_3 = sg[2].s_id)
+        dbManage.db.session.add(ag)
+    if len(students) == 2:
+        ag = ExamGroup(exam_id = exam.exam_id,s_id_1 = students[0].s_id,s_id_2 = students[1].s_id)
+        dbManage.db.session.add(ag)
+    if len(students) == 1:
+        ag = ExamGroup(exam_id = exam.exam_id,s_id_1 = students[0].s_id)
+        dbManage.db.session.add(ag)
+    dbManage.db.session.commit()
+
     return jsonify({'code':200,'message':"添加成功",'data':exam.exam_id})
 
 @createFightRoute.route('/getExam',methods=['POST']) 
@@ -35,61 +55,78 @@ def getExam():
     data = json.loads(data.decode("utf-8"))
 
     class_id = data['class_id']
+    token = data['token']
+    try:
+        s = Serializer('WEBSITE_SECRET_KEY')
+        token_id = s.loads(token)['id']
+        token_role = s.loads(token)['role']
+    except:
+        return jsonify({'code':500,'message':"请求失败",'data':{'data':None,'role':None}})
     exams = Exam.query.filter(Exam.class_id == class_id).all()
     content = []
     for e in exams:
         now_time = datetime.datetime.now()
-        if now_time > e.start_time:
-            e.status = 1
-            dbManage.db.session.commit()
         if now_time > e.end_time:
-            status = 3
             e.status = 3
+            stopExam(e.exam_id)
             dbManage.db.session.commit()
         else:
-            status = e.status
-        temp = {"exam_id":e.exam_id, "title": e.title, "start_time": str(e.start_time), "end_time": str(e.end_time), "status": status}
+            if now_time > e.start_time:
+                e.status = 1
+                pushExam(e.exam_id)
+            else:
+                e.status = 0
+        is_submit = None
+        score = None
+        if token_role == Role.StudentRole:
+            se = StudentExam.query.filter(StudentExam.exam_id == e.exam_id,StudentExam.s_id == token_id).first()
+            if not se:
+                is_submit = 0
+            else:
+                score = se.score
+                is_submit = 1
+        temp = {"exam_id":e.exam_id, "title": e.title, "start_time": str(e.start_time), "end_time": str(e.end_time), "status": e.status,'is_submit':is_submit,'score':score}
         content.append(temp)
-    return jsonify({'code':200,'message':"请求成功",'data':content})
+    return jsonify({'code':200,'message':"请求成功",'data':{'data':content,'role':token_role}})
 
-@createFightRoute.route('/pushExam',methods=['POST']) 
-def pushExam():
-    data = request.get_data()
-    data = json.loads(data.decode("utf-8"))
+def pushExam(exam_id):
 
-    exam_id = data['exam_id']
     exam = Exam.query.filter(Exam.exam_id == exam_id).first()
     exam.status = 1
     dbManage.db.session.commit()
+    return jsonify({'code':200,'message':"请求成功",'data':None})
 
-    # 找到班级学生，生产测验小组
-    class_id = exam.class_id
-    students = StudentClass.query.filter(StudentClass.class_id == class_id).all()
-    while(len(students) >= 3):
-        sg = random.sample(students,3)
-        students.remove(sg[0])
-        students.remove(sg[1])
-        students.remove(sg[2])
-        ag = ExamGroup(exam_id = exam_id,s_id_1 = sg[0].s_id,s_id_2 = sg[1].s_id,s_id_3 = sg[2].s_id)
-        dbManage.db.session.add(ag)
-    if len(students) == 2:
-        ag = ExamGroup(exam_id = exam_id,s_id_1 = students[0].s_id,s_id_2 = students[1].s_id)
-        dbManage.db.session.add(ag)
-    if len(students) == 1:
-        ag = ExamGroup(exam_id = exam_id,s_id_1 = students[0].s_id)
-        dbManage.db.session.add(ag)
+@createFightRoute.route('/pushExam',methods=['POST']) 
+def pushExamForce():
+    data = request.get_data()
+    data = json.loads(data.decode("utf-8"))
+    exam_id = data['exam_id']
+    exam = Exam.query.filter(Exam.exam_id == exam_id).first()
+    exam.status = 1
+    exam.start_time = datetime.datetime.now()
     dbManage.db.session.commit()
+    return jsonify({'code':200,'message':"请求成功",'data':None})
 
+def stopExam(exam_id):
+    
+    exam = Exam.query.filter(Exam.exam_id == exam_id).first()
+    exam.status = 3
+    dbManage.db.session.commit()
+    egs = ExamGroup.query.filter(ExamGroup.exam_id == exam_id).all()
+    for e in egs:
+        dbManage.db.session.delete(e)
+    dbManage.db.session.commit()
     return jsonify({'code':200,'message':"请求成功",'data':None})
 
 @createFightRoute.route('/stopExam',methods=['POST']) 
-def stopExam():
+def stopExamForce():
     data = request.get_data()
     data = json.loads(data.decode("utf-8"))
-
     exam_id = data['exam_id']
     exam = Exam.query.filter(Exam.exam_id == exam_id).first()
     exam.status = 3
+    exam.end_time = datetime.datetime.now()
+    dbManage.db.session.commit()
     egs = ExamGroup.query.filter(ExamGroup.exam_id == exam_id).all()
     for e in egs:
         dbManage.db.session.delete(e)
@@ -119,3 +156,21 @@ def addQuestion():
     dbManage.db.session.commit()
     return jsonify({'code':200,'message':"添加成功"})
 
+
+@createFightRoute.route('/editExamTime',methods=['POST']) 
+def editExamRime():
+    data = request.get_data()
+    data = json.loads(data.decode("utf-8"))
+    exam_id = data['exam_id']
+    start_time = data['start_time']
+    end_time = data['end_time']
+
+    end_time = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
+    start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+
+    exam = Exam.query.filter(Exam.exam_id == exam_id).first()
+    exam.start_time = start_time
+    exam.end_time = end_time
+
+    dbManage.db.session.commit()
+    return jsonify({'code':200,'message':"添加成功"})
--- Routers/ManageFight/getExam.py
+++ Routers/ManageFight/getExam.py
@@ -83,3 +83,48 @@ def submitExam():
     dbManage.db.session.add(se)
     dbManage.db.session.commit()
     return jsonify({'code':200,'message':"提交成功",'data':{'rank':count + 1,'score':score}})
+
+@getExamRoute.route('/getCloseExam',methods=['POST'])
+def getCloseExam():
+    data = request.get_data()
+    data = json.loads(data.decode("utf-8"))
+    exam_id = int(data['exam_id'])
+    s_id = str(data['s_id'])
+    exam = Exam.query.filter(Exam.exam_id == exam_id).first()
+    se = StudentExam.query.filter(and_(StudentExam.exam_id == exam_id, StudentExam.s_id == s_id)).first()
+    questions = Question.query.filter(Question.exam_id == exam_id).all()
+    content=[]
+    answerList = []
+    total_score = 0.0
+    for q in questions:
+        total_score = total_score + q.q_score
+        answer = 0
+        checkList = 0
+        if q.q_type == 1 or q.q_type == 3:
+            answer = int(q.answer)
+        else:
+            checkList = []
+            for i in range(len(q.answer)):
+                checkList.append(int(q.answer[i]))
+        qs = {'title':q.title,'option_a':q.option_a,'option_b':q.option_b,'option_c':q.option_c,'option_d':q.option_d,
+        'q_score':q.q_score,
+        'q_type':q.q_type,'answer':answer,'q_id':q.question_id,'checkList':checkList}
+        content.append(qs)
+        sq = StudentExamQuestion.query.filter(and_(StudentExamQuestion.q_id == q.question_id,StudentExamQuestion.s_id == s_id)).first()
+        if not sq:
+            return jsonify({'code':200,'message':"请求成功",'data':{'questionList':content,'answerList':None,'ex_title':exam.title,'start_time':str(exam.start_time),'end_time':str(exam.end_time),}})
+        choice = sq.choice
+        s_answer = 0
+        s_checkList = []
+        if choice == "":
+            s_answer = 0
+            s_checkList = []
+        else:
+            if q.q_type == 1 or q.q_type == 3:
+                s_answer = str(sq.choice)
+            else:
+                for i in range(len(choice)):
+                    s_checkList.append(int(choice[i]))
+        answer = {'sq_id':q.question_id,'answer':s_answer,'checkList':s_checkList}
+        answerList.append(answer)
+    return jsonify({'code':200,'message':"请求成功",'data':{'questionList':content,'answerList':answerList,'ex_title':exam.title,'ex_score':total_score,'score':se.score,'start_time':str(exam.start_time),'end_time':str(exam.end_time),}})
